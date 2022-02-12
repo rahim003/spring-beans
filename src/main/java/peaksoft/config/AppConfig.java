e4f7f541aefeda2633b34984124680e3cc8251b3
krasa <jhhhk>
2022-02-12 11:52:21 +0300

    Springpractica1

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,12 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Dog;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
 }
---
+++ src/main/java/peaksoft/model/Dog.java
@@ -0,0 +1,14 @@
+package peaksoft.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+
+}
